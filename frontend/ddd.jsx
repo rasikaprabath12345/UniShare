076e128b14624ca6224c1f3705214494d81d9dfb
rasikaprabath12345 <rasikaprabath8694@gmail.com>
2026-06-15 23:12:56 +0530

    fix: allow Vercel and Netlify preview origins in CORS

--- frontend/ddd.jsx
+++ frontend/ddd.jsx
@@ -1,5 +1,5 @@
 import { LensConvex } from "lucide-react"
 
 cbcxbxsdsdvsd x fc c  cv c xcv c v cvcv cvv v cvc cv ccv c   cv  v vdxv c LensConvexvdcbcx vccf
-cc cfbcfcbx bcfbbcfbfxgsgbgdsdgsggssgdbdsbsgsxbbbsbvaagzbgsggsegxdsgggseg
+cc cfbcfcbx cv f
 fhshgeggdshrhcvfd 
